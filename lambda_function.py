44738aa0dfb7cd2334a74497cf7da214221cef0d
Ziegler <18589@icf.com>
2019-07-07 12:16:34 -0400

    write download to S3

--- lambda_function.py
+++ lambda_function.py
@@ -8,6 +8,7 @@ import json
 import sys
 import requests
 import bs4
+import re
 from S3TextFromLambdaEvent import *
 
 
@@ -24,10 +25,13 @@ def lambda_handler(event, context):
 			log = setup_logging("aws-download-webpages", event, aws_request_id)
 
 		if "async" in event:
+			s3 = boto3.resource("s3")
 			url = event["url"]
 			res = download_page(url.strip())
 			result = {"processing_type" : "async download urls", "url" : url, "status_code" : res.status_code, "length" : len(res.text)}
 			log.critical("processed url", result=result)
+			filename = re.sub(r"[^a-zA-Z0-9-_]", "", url)
+			create_s3_text_file("svz-aws-download-webpages", "ouput/" + filename, res.text, s3)
 			return result
 		else:
 			file_data = get_urls_from_file_text(event)
